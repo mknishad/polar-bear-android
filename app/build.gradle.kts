39bbc6087872dd2a84ae630ddc3d9ddba4bb2ee6
Monir <mknishad1@ymail.com>
2025-05-06 04:25:04 +0600

    Add dependency for Paging 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,4 +72,6 @@ dependencies {
   implementation(libs.room.runtime)
   ksp(libs.room.compiler)
   implementation(libs.room.ktx)
+  implementation(libs.paging.runtime)
+  implementation(libs.paging.compose)
 }
