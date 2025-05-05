d47117a0ff72a97e42115b0412832eb7a2b3d7f2
Monir <mknishad1@ymail.com>
2025-05-06 05:29:26 +0600

    Configure DI

---
+++ app/src/main/java/com/mknishad/polarbear/BeerApp.kt
@@ -0,0 +1,7 @@
+package com.mknishad.polarbear
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BeerApp: Application()
---
+++ app/src/main/java/com/mknishad/polarbear/di/AppModule.kt
@@ -0,0 +1,59 @@
+package com.mknishad.polarbear.di
+
+import android.content.Context
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.room.Room
+import com.mknishad.polarbear.data.local.BeerDatabase
+import com.mknishad.polarbear.data.local.BeerEntity
+import com.mknishad.polarbear.data.remote.BeerApi
+import com.mknishad.polarbear.data.remote.BeerRemoteMediator
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+  @Provides
+  @Singleton
+  fun provideBeerDatabase(@ApplicationContext context: Context): BeerDatabase {
+    return Room.databaseBuilder(
+      context,
+      BeerDatabase::class.java,
+      "beers.db"
+    ).build()
+  }
+
+  @Provides
+  @Singleton
+  fun provideBeerApi(): BeerApi {
+    return Retrofit.Builder()
+      .baseUrl(BeerApi.BASE_URL)
+      .addConverterFactory(GsonConverterFactory.create())
+      .build()
+      .create(BeerApi::class.java)
+  }
+
+  @OptIn(ExperimentalPagingApi::class)
+  @Provides
+  @Singleton
+  fun provideBeerPager(beerDb: BeerDatabase, beerApi: BeerApi): Pager<Int, BeerEntity> {
+    return Pager(
+      config = PagingConfig(pageSize = 20),
+      remoteMediator = BeerRemoteMediator(
+        beerDb = beerDb,
+        beerApi = beerApi
+      ),
+      pagingSourceFactory = {
+        beerDb.dao.pagingSource()
+      }
+    )
+  }
+}
