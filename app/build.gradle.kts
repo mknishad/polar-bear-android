2bc737fd4c909efcd407467898ada7b857d1b018
Monir <mknishad1@ymail.com>
2025-05-06 04:18:34 +0600

    Add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
   alias(libs.plugins.android.application)
   alias(libs.plugins.kotlin.android)
   alias(libs.plugins.kotlin.compose)
+  alias(libs.plugins.kotlin.ksp)
+  alias(libs.plugins.hilt)
 }
 
 android {
@@ -53,4 +55,21 @@ dependencies {
   androidTestImplementation(libs.androidx.ui.test.junit4)
   debugImplementation(libs.androidx.ui.tooling)
   debugImplementation(libs.androidx.ui.test.manifest)
+  implementation(libs.coroutines.core)
+  implementation(libs.coroutines.android)
+  //implementation(libs.coroutines.lifecycle.runtime)
+  implementation(libs.coroutines.lifecycle.viewmodel)
+  implementation(libs.dagger.hilt.android)
+  ksp(libs.dagger.hilt.android.compiler)
+  ksp(libs.hilt.compiler)
+  implementation(libs.hilt.navigation.compose)
+  implementation(libs.retrofit2.retrofit)
+  implementation(libs.retrofit2.converter.gson)
+  implementation(libs.okhttp3.okhttp)
+  implementation(libs.okhttp3.logging.interceptor)
+  implementation(libs.coil.compose)
+  implementation(libs.coil.okhttp)
+  implementation(libs.room.runtime)
+  ksp(libs.room.compiler)
+  implementation(libs.room.ktx)
 }
